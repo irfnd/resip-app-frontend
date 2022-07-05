e25dd855c6cf24d871399f641696765fe29e76a9
Irfnd <irfandiabimanyu@gmail.com>
2022-07-05 19:52:34 +0700

    Add - React-router-dom and some routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <div className="App">Hello, World!</div>;
-}
+import { Routes, Route } from "react-router-dom";
 
-export default App;
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
+
+export default function App() {
+	return (
+		<Routes>
+			<Route path="/">
+				<Route index element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="*" element={<NotFound />} />
+			</Route>
+		</Routes>
+	);
+}
