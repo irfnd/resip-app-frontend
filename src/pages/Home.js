66e3a1f9a16672e8822be4b79cab7fa79e78f7ec
Irfnd <irfandiabimanyu@gmail.com>
2022-07-08 17:35:16 +0700

    Update -Fixing row and col Homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,8 @@ import { FiSearch } from "react-icons/fi";
 import img1 from "../images/homepage-img-1.jpg";
 import img2 from "../images/new-recipe-1.jpg";
 
+import recipe1 from "../images/recipes/recipe-1.jpg";
+
 function ImageProfile() {
 	return (
 		<Image
@@ -22,105 +24,183 @@ export default function Home() {
 
 	return (
 		<>
-			<Navbar collapseOnSelect expand="lg" fixed="top" className="bg-glass py-2">
-				<Container className={!login ? "py-3" : "py-2"}>
+			{/* Navbar */}
+			<Navbar collapseOnSelect expand="lg" fixed="top" className="bg-glass py-4">
+				<Container>
 					<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+					<Nav className="d-flex flex-row align-items-center order-lg-2">
+						{!login && (
+							<>
+								<li className="me-2 fw-bold">
+									<Link to="/login">
+										<Button variant="primary" className="text-light p-2 px-4">
+											Log In
+										</Button>
+									</Link>
+								</li>
+								<li>
+									<Link to="/register">
+										<Button variant="warning" className="p-2 px-4">
+											Sign Up
+										</Button>
+									</Link>
+								</li>
+							</>
+						)}
+					</Nav>
+					{login && (
+						<NavDropdown title={<ImageProfile />} align="end" className="order-lg-2 py-1">
+							<NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.2">My Recipes</NavDropdown.Item>
+							<NavDropdown.Divider />
+							<NavDropdown.Item href="#action/3.4">Log Out</NavDropdown.Item>
+						</NavDropdown>
+					)}
 					<Navbar.Collapse id="responsive-navbar-nav">
-						<Nav className="me-auto">
-							<li className="ms-0 p-0 px-5 ps-0 active-nav" active>
+						<Nav className="me-auto pt-4 ps-4 p-lg-0 gap-3 gap-lg-5">
+							<li className="active-nav" active>
 								<Link to="/" className="text-dark fw-bold">
 									Home
 								</Link>
 							</li>
-							<li className="p-0 px-5" active={false}>
+							<li className="" active={false}>
 								<Link to="/" className="text-dark fw-bold">
 									Add Recipe
 								</Link>
 							</li>
-							<li className="me-0 p-0 px-5 pe-0" active={false}>
+							<li className="" active={false}>
 								<Link to="/" className="text-dark fw-bold">
 									Profile
 								</Link>
 							</li>
 						</Nav>
-						<Nav className="d-flex align-items-center">
-							{!login ? (
-								<>
-									<li className="me-2 fw-bold">
-										<Link to="/login">
-											<Button variant="primary" className="text-light p-2 px-4">
-												Log In
-											</Button>
-										</Link>
-									</li>
-									<li>
-										<Link to="/register">
-											<Button variant="warning" className="p-2 px-4">
-												Sign Up
-											</Button>
-										</Link>
-									</li>
-								</>
-							) : (
-								<NavDropdown title={<ImageProfile />} align="end" id="basic-nav-dropdown">
-									<NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
-									<NavDropdown.Item href="#action/3.2">My Recipes</NavDropdown.Item>
-									<NavDropdown.Divider />
-									<NavDropdown.Item href="#action/3.4">Log Out</NavDropdown.Item>
-								</NavDropdown>
-							)}
-						</Nav>
 					</Navbar.Collapse>
 				</Container>
 			</Navbar>
 
-			<div className="bg-primary background-accent-1"> </div>
-
+			{/* Homepage Content */}
 			<Container>
-				{/* Section 1 - Search Bar */}
-				<Row className="vh-100 pt-5">
-					<Col md={6} className="d-flex flex-column justify-content-center">
-						<Row>
-							<Col md={10}>
-								<p className="display-3 fw-medium">Discover Recipe & Delicious Food</p>
-								<div className="input-group mb-3">
-									<span className="input-group-text bg-primary border-primary">
-										<FiSearch size={20} className="text-light" />
-									</span>
-									<input type="text" className="form-control" placeholder="Search food recipe" />
-								</div>
+				<Row className="d-flex flex-column">
+					{/* Section 1 - Search Bar */}
+					<Col className="mt-5 py-5">
+						<Row className="py-5">
+							<Col xs={12} md={6} className="mb-5 m-md-0">
+								<Row className="h-100 d-flex align-items-center">
+									<Col md={10} className="text-center text-md-start">
+										<p className="display-3 fw-medium">Discover Recipe & Delicious Food</p>
+										<div className="input-group mb-3">
+											<span className="input-group-text bg-primary border-primary">
+												<FiSearch size={20} className="text-light" />
+											</span>
+											<input type="text" className="form-control" placeholder="Search food recipe" />
+										</div>
+									</Col>
+								</Row>
+							</Col>
+							<Col xs={12} md={6}>
+								<Row className="h-100 d-flex align-items-center justify-content-end">
+									<Col lg={10}>
+										<Image src={img1} fluid alt="..." rounded />
+									</Col>
+								</Row>
 							</Col>
 						</Row>
 					</Col>
-					<Col md={6} className="d-flex justify-content-end align-items-center">
-						<Image src={img1} width="500" alt="..." rounded />
+
+					{/* Section 2 - New Recipe */}
+					<Col>
+						<Row className="py-5">
+							<Col xs={12} md={6} className="mb-5 m-md-0">
+								<Row className="h-100 d-flex align-items-center">
+									<Col lg={10}>
+										<p className="ts-30 fw-medium tag mb-5">New Recipe</p>
+										<Image src={img2} fluid alt="..." rounded />
+									</Col>
+								</Row>
+							</Col>
+							<Col xs={12} md={6}>
+								<Row className="h-100 d-flex align-items-center justify-content-end">
+									<Col md={10}>
+										<p className="display-6 fw-medium">Healthy Bone Broth Ramen (Quick & Easy)</p>
+										<hr />
+										<p className="ts-20">
+											Quick + Easy Chicken Bone Broth Ramen- Healthy chicken ramen in a hurry? That’s right!
+										</p>
+										<Button variant="warning" className="p-3 px-4">
+											Learn More
+										</Button>
+									</Col>
+								</Row>
+							</Col>
+						</Row>
+					</Col>
+
+					{/* Section 3 - Latest Recipe */}
+					<Col>
+						<Row className="py-5 row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4">
+							<Col sm={12} md={12} lg={12}>
+								<p className="ts-30 fw-medium tag mb-5">Latest Recipe</p>
+							</Col>
+							<Col className="my-3">
+								<Image src={recipe1} fluid alt="..." rounded />
+							</Col>
+							<Col className="my-3">
+								<Image src={recipe1} fluid alt="..." rounded />
+							</Col>
+							<Col className="my-3">
+								<Image src={recipe1} fluid alt="..." rounded />
+							</Col>
+							<Col className="my-3">
+								<Image src={recipe1} fluid alt="..." rounded />
+							</Col>
+							<Col className="my-3">
+								<Image src={recipe1} fluid alt="..." rounded />
+							</Col>
+							<Col className="my-3">
+								<Image src={recipe1} fluid alt="..." rounded />
+							</Col>
+							<Col className="my-3">
+								<Image src={recipe1} fluid alt="..." rounded />
+							</Col>
+							<Col className="my-3">
+								<Image src={recipe1} fluid alt="..." rounded />
+							</Col>
+						</Row>
 					</Col>
 				</Row>
 			</Container>
 
-			<div className="bg-primary background-accent-2"> </div>
-			<Container>
-				<Row className="vh-100 pt-5">
-					<Col md={6} className="d-flex flex-column justify-content-evenly">
-						<p className="ts-30 fw-medium tag">New Recipe</p>
-						<Image src={img2} width="500" alt="..." rounded />
-					</Col>
-					<Col md={6} className="d-flex flex-column justify-content-center align-items-end">
-						<Row className="d-flex flex-column justify-content-center align-items-end">
-							<Col md={10}>
-								<p className="display-6 fw-medium">Healthy Bone Broth Ramen (Quick & Easy)</p>
-								<hr />
-								<p className="ts-20">
-									Quick + Easy Chicken Bone Broth Ramen- Healthy chicken ramen in a hurry? That’s right!
-								</p>
-								<Button variant="warning" className="p-3 px-4">
-									Learn More
-								</Button>
-							</Col>
-						</Row>
-					</Col>
-				</Row>
-			</Container>
+			{/* Footer */}
+			<Row className="bg-primary mt-5 py-5 d-flex justify-content-center align-items-center w-100 m-0">
+				<Col md={12} className="text-center my-5">
+					<p className="display-3 fw-medium mb-4 text-light">Eat, Cook, Repeat</p>
+					<p className="ts-20 text-light">Share your best recipe by uploading here !</p>
+				</Col>
+				<Col md={12} className="mt-5">
+					<Nav className="justify-content-center bg-primary w-100">
+						<Nav.Item>
+							<Nav.Link href="/home" className="text-light">
+								Product
+							</Nav.Link>
+						</Nav.Item>
+						<Nav.Item>
+							<Nav.Link href="/home" className="text-light">
+								Company
+							</Nav.Link>
+						</Nav.Item>
+						<Nav.Item>
+							<Nav.Link href="/home" className="text-light">
+								Learn More
+							</Nav.Link>
+						</Nav.Item>
+						<Nav.Item>
+							<Nav.Link href="/home" className="text-light">
+								Get in Touch
+							</Nav.Link>
+						</Nav.Item>
+					</Nav>
+				</Col>
+			</Row>
 		</>
 	);
 }
